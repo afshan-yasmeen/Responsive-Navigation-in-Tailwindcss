f88dd1335bd11247166c340fd00218b947c9f79c
AfshanYasmeen16 <afshanyasmeen16@gmail.com>
2024-03-31 09:34:11 +0500

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,19 @@ export default function Home() {
  <div>
 <section
 id="hero" className="text-gray-400 h-screen bg-gray-900 text-center flex">
-  <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold ">Hero</h1>
+  <h1 className="m-auto font-waterfall text-7xl text-white font-bold ">Hero</h1>
 </section>
 <section
 id="blogs" className="text-gray-400 h-screen bg-gray-900 text-center flex">
-  <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold ">Blog</h1>
+  <h1 className="m-auto font-waterfall text-7xl text-white font-bold ">Blog</h1>
 </section>
 <section
 id="contact" className="text-gray-400 h-screen bg-gray-900 text-center flex">
-  <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold ">Contact</h1>
+  <h1 className="m-auto font-waterfall text-7xl text-white font-bold ">Contact</h1>
 </section>
 <section
 id="projects" className="text-gray-400 h-screen bg-gray-900 text-center flex">
-  <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold ">Projects</h1>
+  <h1 className="m-auto font-waterfall text-7xl text-white font-bold ">Projects</h1>
 </section>
 
  </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     return (
         <div>
             <nav
-                className='bg-black fixed w-full top-0 left-0 right-0 z-10'>
+                className='bg-blue-900/80 md:bg-blue-900/50 fixed w-full top-0 left-0 right-0 z-10'>
                 <div className='justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8'>
 
                     <div>
@@ -45,25 +45,25 @@ ${navbar ? 'p-12 md:p-0 block' : 'hidden'}
 ` }>
     <ul className='h-screen md:h-auto items-center justify-center md:flex'>
         <li className='pb-6 text-xl text-white py-2 md:px-6 
-        text-center border-b-2 md:border-b-0 hover:bg-purple-600'>
+        text-center border-b-2 md:border-b-0 hover:underline'>
 <Link href={'#about'} onClick={()=>setNavbar(!navbar)}>
     About
 </Link>
         </li>
         <li className='pb-6 text-xl text-white py-2 md:px-6 
-        text-center border-b-2 md:border-b-0 hover:bg-purple-600'>
+        text-center border-b-2 md:border-b-0 hover:underline'>
 <Link href={'#blogs'} onClick={()=>setNavbar(!navbar)}>
     Blog
 </Link>
         </li>
         <li className='pb-6 text-xl text-white py-2 md:px-6 
-        text-center border-b-2 md:border-b-0 hover:bg-purple-600'>
+        text-center border-b-2 md:border-b-0 hover:underline'>
 <Link href={'#contact'} onClick={()=>setNavbar(!navbar)}>
     Contact
 </Link>
         </li>
         <li className='pb-6 text-xl text-white py-2 md:px-6 
-        text-center border-b-2 md:border-b-0 hover:bg-purple-600'>
+        text-center border-b-2 md:border-b-0 hover:underline'>
 <Link href={'#projects'} onClick={()=>setNavbar(!navbar)}>
     Projects
 </Link>
